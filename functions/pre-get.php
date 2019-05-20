36b35b555b099c07e80e978243c741ec492dd92a
Manuel Schmalstieg <git.code@ms-studio.net>
2019-05-20 17:28:24 +0200

    Page des membres: afficher TOUS les membres

--- functions/pre-get.php
+++ functions/pre-get.php
@@ -10,8 +10,10 @@
 function cowork_page_membres( $query ) {
 
   if ( is_post_type_archive( 'cwn_fiche' ) && !is_admin() && $query->is_main_query() ) {
-  	// $query->set( 'posts_per_page', 1);
+
   	$query->set( 'orderby', 'rand');
+  	$query->set( 'posts_per_page', -1);
+
   }
  
 }
