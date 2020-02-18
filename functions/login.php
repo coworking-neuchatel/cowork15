b2f37720b70d02b67148f4d1000451cf75a17d23
Manuel Schmalstieg <git.code@ms-studio.net>
2020-02-18 22:32:42 +0100

    Remplacement d'une fonction obsolète

--- functions/login.php
+++ functions/login.php
@@ -25,4 +25,4 @@ add_filter( 'login_headerurl', 'cowork_login_logo_url' );
 function cowork_login_logo_url_title() {
     return 'Coworking Neuchâtel';
 }
-add_filter( 'login_headertitle', 'cowork_login_logo_url_title' );
+add_filter( 'login_headertext', 'cowork_login_logo_url_title' );
